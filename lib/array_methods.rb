7f50aaaad52e74aba07f378483ea80de5ceb0185
Christopher Leja <christopher.leja@gmail.com>
2020-01-27 16:48:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0 
   while counter < array.length do
-    if (array[counter]==value_to_find)
+    if (array[counter].include?(value_to_find))
+      puts array[counter]
       counter += 1
     end
-    puts array[counter]
   end
   
 end
