3f6b72283def840cb5097298d084c3e63d02ac2f
Christopher Leja <christopher.leja@gmail.com>
2020-01-27 16:55:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -5,8 +5,7 @@ def find_element_index(array, value_to_find)
     array.include?(value_to_find)
       if true
         p array[i]
-      end
-    else puts "value not in index"
+      else puts "value not in index"
 end
 
 def find_max_value(array)
